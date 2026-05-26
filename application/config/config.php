08958ef198dbc8e8e24eac18e89fc59f4d586712
GiB07 <ahgeb15@gmail.com>
2026-05-26 15:26:31 +0800

    config giusob

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/Tattoo_Shop/';
+// $config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/Tattoo_Shop/';
+$config['base_url'] = 'https://ggwp.gt.tc.infinityfreeapp.com/';
 
 /*
 |--------------------------------------------------------------------------
